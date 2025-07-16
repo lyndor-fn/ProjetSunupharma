57131c7e3567c75fde669a7bfc17830b9e4d4ed4
Souadouuuu <souaddiallo00@gmail.com>
2025-07-16 20:32:39 +0100

    Update and rename categorie.h to categories.h

--- gestioncategories/categories.h
+++ gestioncategories/categories.h
@@ -1,13 +1,13 @@
 
-
-#ifndef CATEGORIE_H
-#define CATEGORIE_H
+#ifndef CATEGORIES_H
+#define CATEGORIES_H
 
 typedef struct {
     int id;
     char libelle[50];
 } Categorie;
 
+void menuGestionCategories();
 void ajouterCategorie();
 void afficherCategories();
 void rechercherCategorie();

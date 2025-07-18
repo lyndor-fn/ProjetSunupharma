24e398153ca952662b06972762a125f73e8c0ee5
boussooo <mametoutybousso2104@gmail.com>
2025-07-18 19:56:15 +0000

    Update main.c

--- main.c
+++ main.c
@@ -1,34 +1,16 @@
-<<<<<<< HEAD
+
 #include <stdio.h>
 #include <stdlib.h>
-<<<<<<< HEAD
 #include "produit.h"
 #include "categories.h"
 #include "utilisateurs.h"
 #include "ventes.h"
 #include "rapports.h"
-=======
-//#include "produits.h"
-//#include "categories.h"
-#include "utilisateurs.h"
-//#include "ventes.h"
-//#include "rapports.h"
->>>>>>> 844778b0052d396ad8e8cec2386a55730f395a34
 
 
 int main() {
 
 //ajouterUtilisateur();
 //afficherUtilisateurs();
-loginUtilisateur();
-<<<<<<< HEAD
-=======
-=======
-#include "rapports.h"
 
-int main() {
-    genererRapportJournalier();
-    return 0;
->>>>>>> lindor
->>>>>>> 844778b0052d396ad8e8cec2386a55730f395a34
 }

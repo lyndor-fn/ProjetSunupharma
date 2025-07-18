ee3781cd8a04332bd5373cba2a35ed0861a87290
feindaaa <fendakanoute222@gmail.com>
2025-07-18 00:32:05 +0000

    Update header.h

--- header.h
+++ header.h
@@ -1,27 +1,37 @@
-#ifndef HEADER_H
-#define HEADER_H
+#ifndef PRODUITS_H
+#define PRODUITS_H
 
 #define MAX_PRODUCTS 100
 #define CODE_LENGTH 6
-#define NOM_LENGTH 50
+#define NAME_LENGTH 50
 #define CATEGORY_LENGTH 30
 
 typedef struct {
     char code[CODE_LENGTH];
-    char designation[NOM_LENGTH];
+    char designation[NAME_LENGTH];
     float prix;
     int quantite;
     char categorie[CATEGORY_LENGTH];
     char date_peremption[11];
 } Produit;
 
-
-void ajouterProduit();
-void modifierProduit();
-void supprimerProduit();
+// Fonctions principales
+void menuGestionProduits();
 void afficherProduits();
-void rechercherProduitParCode();
-void mettreAJourStock();
+
+// Fonctions de gestion
+void ajouterEtSauvegarderProduit();
+void modifierEtSauvegarderProduit();
+void supprimerEtSauvegarderProduit();
+
+// Fonctions de persistance
+void chargerProduitsDepuisFichier();
 void sauvegarderProduits();
-void chargerProduits();
+
+// Fonctions utilitaires
+int estCodeValide(const char* code);
+int estDateValide(const char* date);
+void afficherSucces(const char* message);
+void afficherErreur(const char* message);
+
 #endif
